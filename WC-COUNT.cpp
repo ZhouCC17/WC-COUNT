cda226041c91bc30c52c517e09dd2e1690d8a808
ZhouCC17 <61728972+ZhouCC17@users.noreply.github.com>
2020-03-15 14:47:25 +0800

    logs

--- WC-COUNT.cpp
+++ WC-COUNT.cpp
@@ -38,8 +38,8 @@ void Word(char File[]){                //单词数统计函数
 				   }while((c>='A'&&c<='Z')||(c>='a'&&c<='z'));      //通过检测是否是连续字符对单词计数 
                 }
         }
-        printf("此文件的单词数为:%d\n",num);
         fclose(f);
+        printf("此文件的单词数为:%d\n",num);
         system("pause");
 }
 
@@ -65,6 +65,40 @@ void Line(char File[]){
         printf("此文件的行数为:%d\n",num);
         system("pause");
 }
+                       
+void Other(char File[]){ 
+        int num1=0;        //注释行行数
+		int num2=0;       //空行行数
+		int num3=0;       //代码行行数 
+        char c[100];
+        FILE *f=fopen(File,"r");
+        if(!f){
+                printf("打开文件出错，请重试！\n");
+                return ;
+        }
+        while(!feof(f)){
+                fgets(c,100,f);
+                if(strstr(c,"//"))     //注释不同形式的处理形式 
+				num1++;
+				else if(strstr(c,"/*")) {
+					do{
+						num1++;
+						fgets(c,100,f);
+					}while(!strstr(c,"*/"));
+					num1++;	
+				}
+                else if(strlen(c)<=2)  //统计字符数确认是否是空行 
+                num2++;
+                else
+                num3++;    //不是其他种类则为代码行 
+            }
+        fclose(f);
+        printf("此文件的注释行行数为:%d\n",num1);
+        printf("此文件的空行行数为:%d\n",num2);
+        printf("此文件的代码行行数为:%d\n",num3);
+        system("pause");
+    }
+
 
 int main()
 {
@@ -72,15 +106,16 @@ int main()
     char file[50];              //记录文件地址
     while(1)
     {
-        printf("==================================\n");
+        printf("====================================\n");
         printf("      WC-COUNT功能参数如下：\n");
         printf("wc.exe-c=========>      统计字符数\n");
         printf("wc.exe-w=========>      统计单词数\n");
         printf("wc.exe-l=========>      统计行数\n");
-        printf("==================================\n");
+        printf("wc.exe-o=========>      统计其他情况\n");
+        printf("====================================\n");
 		printf("请输入命令参数wc.exe-");
         scanf(" %c",&parameter);
-        while(parameter!='c'&&parameter!='w'&&parameter!='l'){
+        while(parameter!='c'&&parameter!='w'&&parameter!='l'&&parameter!='o'){
             printf("输入参数出错，请重新输入:");
             scanf(" %c",&parameter); 
         }
@@ -90,6 +125,7 @@ int main()
                 case 'c': Character(file);   break;
                 case 'w': Word(file);    break;
                 case 'l': Line(file);    break;
+                case 'o': Other(file);   break;
                 default :   break;                                
         }
     }
